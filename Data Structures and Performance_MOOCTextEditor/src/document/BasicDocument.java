26d66c2c80340eeab63afe8e725568b3fbbd1494
Khoa Tran <trandangkhoa@tma.com.vn>
2017-07-13 07:58:35 +0700

    Introduce count syllable per word method

--- Data Structures and Performance_MOOCTextEditor/src/document/BasicDocument.java
+++ Data Structures and Performance_MOOCTextEditor/src/document/BasicDocument.java
@@ -2,7 +2,6 @@ package document;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -80,31 +79,68 @@ public class BasicDocument extends Document
 	public int getNumSyllables()
 	{
 		String text = this.getText();
-		int count = 0;
-		for( int i = 0 ; i < text.length() ; i++)
-		{
-			char c = text.charAt(i);
-			if(!syllableSet.contains(c))
-			{
-				continue;
-			}
-			if(count != 0 && c == 'e' && i == text.length() -1)
-			{
-				break;
-			}
-			count++;
-			for(int j = i + 1; j < text.length() ; j++)
-			{
-				char nextC = text.charAt(j);
-				if(!syllableSet.contains(nextC))
-				{
-					i = j;
-					break;
-				}
-			}
-		}
-		return count;
+		int total = 0;
+        int startWord = 0;
+
+        for(int i = 0 ; i < text.length() ; i++)
+        {
+            String word = null;
+            int end = 0;
+            if(periodSet.contains(text.charAt(i)))
+            {
+                end = i ;
+                if(startWord == end)
+                {
+                    continue;
+                }
+                word = text.substring(startWord,end);
+                total += countSyllable(word);
+                startWord = end + 1;
+            }
+            else if( i == text.length() - 1)
+            {
+                end = i+1;
+                word = text.substring(startWord,end);
+                total += countSyllable(word);
+            }
+        }
+
+		return total;
 	}
+
+	private static int countSyllable(String word)
+    {
+        boolean containVow = false;
+        int count = 0;
+        for(int i = 0 ; i < word.length(); i++)
+        {
+            char ch = word.charAt(i);
+            if(syllableSet.contains(ch))
+            {
+                containVow = true;
+                for(int j = i + 1; j < word.length() ; j++)
+                {
+                	i = j;
+                    char chJ = word.charAt(j);
+                    if(!syllableSet.contains(chJ))
+                    {
+                        break;
+                    }
+                }
+                count++;
+            }
+        }
+        if(containVow && word.length() > 1)
+        {
+            char secondLastCh = word.charAt(word.length() - 2);
+            char lastCh = word.charAt(word.length() - 1);
+            if(count > 1 && (!syllableSet.contains(secondLastCh) && (lastCh == 'e' || lastCh == 'E')))
+            {
+                count--;
+            }
+        }
+        return count;
+    }
 
 	/**
 	 * Count how many time a pattern is matched.
@@ -133,30 +169,56 @@ public class BasicDocument extends Document
 		 * in the string, respectively.  You can use these examples to help clarify 
 		 * your understanding of how to count syllables, words, and sentences.
 		 */
-		testCase(new BasicDocument("Segue"), 2, 1, 1);
-		testCase(new BasicDocument("Sentence"), 2, 1, 1);
-		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
-		testCase(new BasicDocument("This is a test.  How many???  "
-		        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
-				16, 13, 5);
-		testCase(new BasicDocument(""), 0, 0, 0);
+
 		testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
 		        + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
-		testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
+
 		testCase(new BasicDocument("Here is a series of test sentences. Your program should "
 				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
 				+ "the correct amount of syllables (example, for example), "
 				+ "but most of them will."), 49, 33, 3);
+
+
 		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
-		         32, 15, 1);
+				32, 15, 1);
+
+		testCase(new BasicDocument("This is a test.  How many???  "
+						+ "Senteeeeeeeeeences are here... there should be 5!  Right?"),
+				16, 13, 5);
+        testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
+
+        testCase(new BasicDocument("Sentence"), 2, 1, 1);
+
+		testCase(new BasicDocument("Segue"), 2, 1, 1);
+
+        testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
+
+        testCase(new BasicDocument(""), 0, 0, 0);
+
 	}
 
 	private static Set<Character> syllableSet =
 			                      new HashSet<Character>(Arrays.asList(new Character('a'),
-			                                                           new Character('e'),
-																	   new Character('i'),
-																	   new Character('o'),
-																	   new Character('u')));
+			                                                   new Character('e'),
+                                                               new Character('i'),
+                                                               new Character('o'),
+															   new Character('u'),
+                                                               new Character('y'),
+                                                               new Character('A'),
+                                                               new Character('E'),
+                                                               new Character('I'),
+                                                               new Character('O'),
+                                                               new Character('U'),
+                                                               new Character('Y')));
+
+    private static Set<Character> periodSet =
+            new HashSet<Character>(Arrays.asList(new Character(' '),
+                    new Character('.'),
+					new Character(','),
+                    new Character('?'),
+                    new Character('!'),
+                    new Character(')')));
+
     private static String PATTERN_SYLLABLE = "[aAeEiIoOuU]{1,}";
 	private static String PATTERN_WORD = "[a-zA-Z]{1,}";
 	private static String PATTERN_END_SENTENCE = "(\\w+)$|[!|\\.|\\?]{1,}";
