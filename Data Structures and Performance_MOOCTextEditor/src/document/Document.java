e25af71151ee483583132d37f65be8f370ebff00
khoat <khoa.tran@gbst.com>
2021-01-12 20:05:20 +1100

    Implement week 2 assignment

--- Data Structures and Performance_MOOCTextEditor/src/document/Document.java
+++ Data Structures and Performance_MOOCTextEditor/src/document/Document.java
@@ -1,10 +1,11 @@
 package document;
 
-/** 
+/**
  * A class that represents a text document
  * @author UC San Diego Intermediate Programming MOOC team
  */
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,7 +13,7 @@ import java.util.regex.Pattern;
 public abstract class Document {
 
 	private String text;
-	
+
 	/** Create a new document from the given text.
 	 * Because this class is abstract, this is used only from subclasses.
 	 * @param text The text of the document.
@@ -21,7 +22,7 @@ public abstract class Document {
 	{
 		this.text = text;
 	}
-	
+
 	/** Returns the tokens that match the regex pattern from the document 
 	 * text string.
 	 * @param pattern A regular expression string specifying the 
@@ -34,27 +35,27 @@ public abstract class Document {
 		ArrayList<String> tokens = new ArrayList<String>();
 		Pattern tokSplitter = Pattern.compile(pattern);
 		Matcher m = tokSplitter.matcher(text);
-		
+
 		while (m.find()) {
 			tokens.add(m.group());
 		}
-		
+
 		return tokens;
 	}
-	
+
 	/** This is a helper function that returns the number of syllables
 	 * in a word.  You should write this and use it in your 
 	 * BasicDocument class.
-	 * 
+	 *
 	 * You will probably NOT need to add a countWords or a countSentences 
 	 * method here.  The reason we put countSyllables here because we'll 
 	 * use it again next week when we implement the EfficientDocument class.
-	 * 
+	 *
 	 * For reasons of efficiency you should not create Matcher or Pattern 
 	 * objects inside this method. Just use a loop to loop through the 
 	 * characters in the string and write your own logic for counting 
 	 * syllables.
-	 * 
+	 *
 	 * @param word  The word to count the syllables in
 	 * @return The number of syllables in the given word, according to 
 	 * this rule: Each contiguous sequence of one or more vowels is a syllable, 
@@ -65,13 +66,38 @@ public abstract class Document {
 	protected int countSyllables(String word)
 	{
 		// TODO: Implement this method so that you can call it from the 
-	    // getNumSyllables method in BasicDocument (module 2) and 
-	    // EfficientDocument (module 3).
-	    return 0;
+		// getNumSyllables method in BasicDocument (module 2) and
+		// EfficientDocument (module 3).
+		List<Character> vowels = Arrays.asList('a','i','o','u','y','w','e','A','I','O','U','Y','W','E');
+		int count = 0;
+		int i = 0;
+		while(i < word.length()) {
+			char c = word.charAt(i);
+			if(vowels.contains(c)) {
+				int start = i;
+				while(vowels.contains(word.charAt(start))) {
+					start++;
+					if(start == word.length())
+						break;
+				}
+				i = start;
+				count++;
+				continue;
+			}
+			i++;
+		}
+		if(count > 1) {
+			char lastChar = word.charAt(word.length() - 1);
+			char secondLastChar = word.charAt(word.length() - 2);
+			if( lastChar == 'e' && !vowels.contains(secondLastChar)) {
+				count--;
+			}
+		}
+		return count;
 	}
-	
+
 	/** A method for testing
-	 * 
+	 *
 	 * @param doc The Document object to test
 	 * @param syllables The expected number of syllables
 	 * @param words The expected number of words
@@ -87,21 +113,21 @@ public abstract class Document {
 		int wordsFound = doc.getNumWords();
 		int sentFound = doc.getNumSentences();
 		if (syllFound != syllables) {
-			System.out.println("\nIncorrect number of syllables.  Found " + syllFound 
+			System.out.println("\nIncorrect number of syllables.  Found " + syllFound
 					+ ", expected " + syllables);
 			passed = false;
 		}
 		if (wordsFound != words) {
-			System.out.println("\nIncorrect number of words.  Found " + wordsFound 
+			System.out.println("\nIncorrect number of words.  Found " + wordsFound
 					+ ", expected " + words);
 			passed = false;
 		}
 		if (sentFound != sentences) {
-			System.out.println("\nIncorrect number of sentences.  Found " + sentFound 
+			System.out.println("\nIncorrect number of sentences.  Found " + sentFound
 					+ ", expected " + sentences);
 			passed = false;
 		}
-		
+
 		if (passed) {
 			System.out.println("passed.\n");
 		}
@@ -110,31 +136,31 @@ public abstract class Document {
 		}
 		return passed;
 	}
-	
-	
+
+
 	/** Return the number of words in this document */
 	public abstract int getNumWords();
-	
+
 	/** Return the number of sentences in this document */
 	public abstract int getNumSentences();
-	
+
 	/** Return the number of syllables in this document */
 	public abstract int getNumSyllables();
-	
+
 	/** Return the entire text of this document */
 	public String getText()
 	{
 		return this.text;
 	}
-	
+
 	/** return the Flesch readability score of this document */
 	public double getFleschScore()
 	{
-	    // TODO: You will play with this method in week 1, and 
+		// TODO: You will play with this method in week 1, and
 		// then implement it in week 2
-	    return 0.0;
+		return 0.0;
 	}
-	
-	
-	
+
+
+
 }
