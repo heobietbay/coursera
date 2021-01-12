1f8b7af5909a5f145d876f2efae327281657c43b
khoat <khoa.tran@gbst.com>
2021-01-12 20:37:25 +1100

    Implement week 2 part 2 (optional)

--- Data Structures and Performance_MOOCTextEditor/src/document/Document.java
+++ Data Structures and Performance_MOOCTextEditor/src/document/Document.java
@@ -158,7 +158,10 @@ public abstract class Document {
 	{
 		// TODO: You will play with this method in week 1, and
 		// then implement it in week 2
-		return 0.0;
+		int wordCount = getNumWords();
+		int sentCount = getNumSentences();
+		int sylcount = getNumSyllables();
+		return 206.835 - ( 1.015 * (wordCount/sentCount) ) - ( 84.6 * ( sylcount/wordCount ) );
 	}
 
 
